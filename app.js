369edcb7cc909dc0473cd0b412545fa8c444437d
Furqanshah77 <shahfurqan668@gmail.com>
2023-12-08 21:20:39 +0500

    JavaScript-Task-2

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+// // Task Two
+
+
+// var value = prompt("Enter Your Value");
+// var string = value.toLowerCase();
+// var len = string.length;
+// var massege = "It Is A Palindrome";
+
+// for (var i = 0; i < len / 2; i++); {
+//     if (string[i] != string[len - 1 - i]) {
+//         massege = "It Is Not A Palindrome";
+//         // break;
+//     }
+// }
+// alert(`${string} : ${massege}`);
+
+
+
+
+
+
+
+
+
+// // Task Three
+
+
 // let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 // let evenNumber = [];
