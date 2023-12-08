a6f77f08adce9bb120a6151c52de60a07fefd00d
Furqanshah77 <shahfurqan668@gmail.com>
2023-12-08 21:12:20 +0500

    javaScript-Task-3

---
+++ app.js
@@ -0,0 +1,21 @@
+// let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// let evenNumber = [];
+// let oddNumber = [];
+
+// for (let i = 0; i < numberArray.length; i++) {
+
+//     if (numberArray[i] % 2 == 0) {
+//         evenNumber.push(numberArray[i]);
+//     } else {
+//         oddNumber.push(numberArray[i]);
+//     }
+
+// }
+
+// document.write("Even Numbers: " + evenNumber.join(", ") + "<br>");
+// document.write("Odd Numbers: " + oddNumber.join(", "));
+
+
+// console.log("Even Numbers: " + evenNumber.join(", "));
+// console.log("Odd Numbers: " + oddNumber.join(", "));
